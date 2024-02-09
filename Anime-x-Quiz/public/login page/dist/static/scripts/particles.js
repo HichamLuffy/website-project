6715eea7b6547b3ad9c2b54dcabad522cc1e8498
HichamLuffy <imalexvssteve2020@gmail.com>
2024-02-09 08:07:39 +0100

    update particles

--- Anime-x-Quiz/public/login page/dist/static/scripts/particles.js
+++ Anime-x-Quiz/public/login page/dist/static/scripts/particles.js
@@ -1,32 +1,32 @@
 $(document).ready(function () {
   // Function to add snow
-  function addSnow () {
+function addSnow () {
     const snowflake = '<div class="snow"></div>';
     $('body').append(snowflake);
-  }
+}
 
   // Function to delete snow
-  function deleteSnow () {
+function deleteSnow () {
     $('.snow:last').remove();
-  }
+}
 
   // Function to clear snow
-  function clearSnow () {
+function clearSnow () {
     $('.snow').remove();
-  }
+}
 
   // Add snow on button click
-  $('#add-snow').click(function () {
+$('.add-snow').click(function () {
     addSnow();
-  });
+});
 
   // Delete snow on button click
-  $('#delete-snow').click(function () {
+$('.delete-snow').click(function () {
     deleteSnow();
-  });
+});
 
   // Clear snow on button click
-  $('#clear-snow').click(function () {
+$('.clear-snow').click(function () {
     clearSnow();
-  });
+});
 });
