f8b01e53a2496c21ff5484ae2883c9398d835d59
HichamLuffy <imalexvssteve2020@gmail.com>
2024-02-12 19:17:22 +0100

    create layout

--- Anime-x-Quiz/project_mvp/Aquiz.py
+++ Anime-x-Quiz/project_mvp/Aquiz.py
@@ -14,6 +14,13 @@ connection = pymysql.connect(
     database='AQZ',
     cursorclass=pymysql.cursors.DictCursor
 )
+page = [
+            {
+                'Home':'Home',
+                'posts': 'Posts',
+                'Quiz': 'Quiz'
+            }
+        ]
 @app.route('/')
 @app.route('/main')
 def about():
@@ -24,7 +31,7 @@ def posts_page():
     with connection.cursor() as cursor:
         cursor.execute('SELECT * FROM posts')
         posts = cursor.fetchall()
-    return render_template('posts.html', posts=posts)
+    return render_template('posts.html', posts=posts, page=page)
 
 if __name__ in '__main__':
     app.run(debug=True)
