360c6ad9d2d03762886e834bf8634d37bf3fdae9
HichamLuffy <imalexvssteve2020@gmail.com>
2024-02-12 19:55:11 +0100

    fix footer

--- Anime-x-Quiz/project_mvp/Aquiz.py
+++ Anime-x-Quiz/project_mvp/Aquiz.py
@@ -14,24 +14,17 @@ connection = pymysql.connect(
     database='AQZ',
     cursorclass=pymysql.cursors.DictCursor
 )
-page = [
-            {
-                'Home':'Home',
-                'posts': 'Posts',
-                'Quiz': 'Quiz'
-            }
-        ]
 @app.route('/')
 @app.route('/main')
 def about():
-    return render_template('main.html')
+    return render_template('main.html', page='main')
 
 @app.route('/main/posts')
 def posts_page():
     with connection.cursor() as cursor:
         cursor.execute('SELECT * FROM posts')
         posts = cursor.fetchall()
-    return render_template('posts.html', posts=posts, page=page)
+    return render_template('posts.html', posts=posts, page='posts')
 
 if __name__ in '__main__':
     app.run(debug=True)
