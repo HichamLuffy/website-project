a7a03df72614cc10affe409accc9d28d3398cb02
HichamLuffy <imalexvssteve2020@gmail.com>
2024-02-16 13:42:41 +0100

    add methods

--- Anime-x-Quiz/project_mvp/Aquiz.py
+++ Anime-x-Quiz/project_mvp/Aquiz.py
@@ -8,8 +8,76 @@ from flask_sqlalchemy import SQLAlchemy
 import pymysql.cursors
 
 app = Flask(__name__)
-
 app.config['SECRET_KEY'] = '5e930833f20b0d4a5fa7505d70f5aa80'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+class User (db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    profile = db.relationship('Profile', backref='user', lazy=True)
+    score = db.relationship('Score', backref='user', lazy=True)
+    created_at = db.Column(db.DateTime, nullable=False)
+    updated_at = db.Column(db.DateTime, nullable=False)
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}, '{self.profile}, '{self.score}')"
+
+
+class Profile (db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    full_name = db.Column(db.String(30), nullable=False)
+    avatar = db.Column(db.String(20), nullable=False, default='default.jpg')
+    bio = db.Column(db.Text, nullable=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('User.id'), nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False)
+    updated_at = db.Column(db.DateTime, nullable=False)
+    def __repr__(self):
+        return f"Profile('{self.full_name}', '{self.avatar}, '{self.bio}')"
+
+
+class Score (db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_answewr = db.Column(db.Text, nullable=False)
+    is_correct = db.Column(db.Boolean, nullable=False)
+    score = db.Column(db.Integer, nullable=False)
+    question_id = db.Column(db.Integer, db.ForeignKey('Question.id'), nullable=False)
+    quiz_id = db.Column(db.Integer, db.ForeignKey('Quiz.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('User.id'), nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False)
+    updated_at = db.Column(db.DateTime, nullable=False)
+    def __repr__(self):
+        return f"Score('{self.score}')"
+
+
+class Quiz (db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    category = db.Column(db.String(100), nullable=False)
+    level = db.Column(db.String(100), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('User.id'), nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False)
+    updated_at = db.Column(db.DateTime, nullable=False)
+    def __repr__(self):
+        return f"Quiz('{self.title}')"
+
+
+class Question (db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    question = db.Column(db.Text, nullable=False)
+    image = db.Column(db.String(20), nullable=True)
+    option_1 = db.Column(db.Text, nullable=False)
+    option_2 = db.Column(db.Text, nullable=False)
+    option_3 = db.Column(db.Text, nullable=False)
+    option_4 = db.Column(db.Text, nullable=False)
+    correct_answer = db.Column(db.Text, nullable=False)
+    quiz_id = db.Column(db.Integer, db.ForeignKey('Quiz.id'), nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False)
+    updated_at = db.Column(db.DateTime, nullable=False)
+    def __repr__(self):
+        return f"Question('{self.question}, {self.image}, {self.option_1}, {self.option_2}, {self.option_3}, {self.option_4}')"
+
 
 connection = pymysql.connect(
     host='localhost',
