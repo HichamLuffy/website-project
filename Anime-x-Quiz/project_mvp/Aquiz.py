c7b0cb152a1e45e5e0d7cda197f4f5c924ff11dc
HichamLuffy <imalexvssteve2020@gmail.com>
2024-02-17 06:29:56 +0100

    add models

--- Anime-x-Quiz/project_mvp/Aquiz.py
+++ Anime-x-Quiz/project_mvp/Aquiz.py
@@ -7,83 +7,12 @@ from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegisterForm, LoginForm
 from flask_sqlalchemy import SQLAlchemy
 import pymysql.cursors
+from models import User, Profile, Score, Quiz, Question, option
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5e930833f20b0d4a5fa7505d70f5aa80'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
-
-class User (db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(60), nullable=False)
-    quiz = db.relationship('Quiz', backref='user', lazy=True)
-    profile = db.relationship('Profile', backref='user', lazy=True)
-    score = db.relationship('Score', backref='user', lazy=True)
-    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    updated_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    def __repr__(self):
-        return f"User('{self.username}', '{self.email}, '{self.profile}, '{self.score}')"
-
-
-class Profile (db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    full_name = db.Column(db.String(30), nullable=False)
-    avatar = db.Column(db.String(20), nullable=False, default='default.jpg')
-    bio = db.Column(db.Text, nullable=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    updated_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    def __repr__(self):
-        return f"Profile('{self.full_name}', '{self.avatar}, '{self.bio}')"
-
-
-class Score (db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    user_answewr = db.Column(db.Text, nullable=False)
-    is_correct = db.Column(db.Boolean, nullable=False)
-    score = db.Column(db.Integer, nullable=False)
-    question_id = db.Column(db.Integer, db.ForeignKey('question.id'), nullable=False)
-    quiz_id = db.Column(db.Integer, db.ForeignKey('quiz.id'), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    updated_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    def __repr__(self):
-        return f"Score('{self.score}')"
-
-
-class Quiz (db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    category = db.Column(db.String(100), nullable=False)
-    level = db.Column(db.String(100), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    updated_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    def __repr__(self):
-        return f"Quiz('{self.title}', '{self.category}', '{self.level}', '{self.created_at}') "
-
-
-class Question(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    question_text = db.Column(db.Text, nullable=False)
-    quiz_id = db.Column(db.Integer, db.ForeignKey('quiz.id'), nullable=False)
-    created_at = db.Column(db.DateTime, nullable=False)
-    updated_at = db.Column(db.DateTime, nullable=False)
-    options = db.relationship('Option', backref='question', lazy=True)
-
-    def __repr__(self):
-        return f"Question('{self.question_text}')"
-
-class Option(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    text = db.Column(db.Text, nullable=False)
-    is_correct = db.Column(db.Boolean, nullable=False)
-    question_id = db.Column(db.Integer, db.ForeignKey('question.id'), nullable=False)
-
-    def __repr__(self):
-        return f"Option('{self.text}', '{self.is_correct}')"
 
 
 connection = pymysql.connect(
