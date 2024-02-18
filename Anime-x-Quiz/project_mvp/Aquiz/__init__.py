a6f3a976052b72c6f27b30bf3c1177429d286994
HichamLuffy <imalexvssteve2020@gmail.com>
2024-02-18 15:14:30 +0100

    login manager

--- Anime-x-Quiz/project_mvp/Aquiz/__init__.py
+++ Anime-x-Quiz/project_mvp/Aquiz/__init__.py
@@ -5,6 +5,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 
 app = Flask(__name__)
@@ -12,5 +13,8 @@ app.config['SECRET_KEY'] = '5e930833f20b0d4a5fa7505d70f5aa80'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+# login_manager.login_view = 'login_page'
+# login_manager.login_message_category = 'info'
 
 from Aquiz import routes
